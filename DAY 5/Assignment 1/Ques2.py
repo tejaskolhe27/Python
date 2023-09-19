427362f26904330efe6e200b5a507cafd75f936d
tejaskolhe27 <kolhetebh@gmail.com>
2023-09-19 23:44:10 +0530

    day5

---
+++ DAY 5/Assignment 1/Ques2.py
@@ -0,0 +1,5 @@
+list1 = ["M", "na", "i", "Raj"]
+list2 = ["y", "me", "s", "esh"]
+print(list1)
+for i in range(len(list1)):
+    print(list1[i],list2[i],sep="",end=" ")
---
+++ DAY 5/Assignment 1/Ques6.py
@@ -0,0 +1,6 @@
+list1 = ["Ashish", "", "Atharva", "Amit", "", "Revati"]
+for i in list1:
+    if i=="":
+        list1.remove(i)
+print(list1)
+
